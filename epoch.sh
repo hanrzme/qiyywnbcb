c7e4cd18f6b9cd3bb921bfc7a901118df25769f4
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2024-12-12 06:38:14 +0800

    Update epoch.sh

--- epoch.sh
+++ epoch.sh
@@ -4,7 +4,7 @@ Pre="0"
 Interval="3600"
 
 Current() {
-  epoch=`curl -sSL "https://rpc.qubic.org/v1/latest-stats" |grep -o '"epoch":[0-9]\+' |head -n1 |cut -d':' -f2`
+  epoch=`wget --no-check-certificate -qO- "https://rpc.qubic.org/v1/latest-stats" |grep -o '"epoch":[0-9]\+' |head -n1 |cut -d':' -f2`
   [ -n "$epoch" ] && [ "$epoch" -gt "0" ] || epoch="0"
   echo -n "$epoch"
 }
